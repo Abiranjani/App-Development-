f3247ae5e80291480a4a6a79e5dadf162e2fc197
Abiranjani <110535764+Abiranjani@users.noreply.github.com>
2023-10-13 14:14:36 +0530

    Add files via upload

---
+++ day 18/src/Review.js
@@ -0,0 +1,225 @@
+import React from 'react'
+import './Review.css';
+import { Link } from 'react-router-dom';
+
+function Review() {
+  return (
+<html>
+
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" href="styles.css" />
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+</head>
+<body>
+
+    <section id="testimonials">
+
+        <div class="testimonial-heading">
+            <span>Comments</span>
+            <h1>Clients Says</h1>
+        </div>
+   
+        <div class="testimonial-box-container">
+         
+            <div class="testimonial-box">
+        
+                <div class="box-top">
+         
+                    <div class="profile">
+             
+                        <div class="profile-img">
+                            <img src="https://cdn-icons-png.flaticon.com/512/168/168727.png" />
+                        </div>
+           
+                        <div class="name-user">
+                            <strong>modi</strong>
+                            <span>@modi</span>
+                        </div>
+                    </div>
+                 
+                    <div class="reviews">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="far fa-star"></i>
+                    </div>
+                </div>
+     
+                <div class="client-comment">
+                    <p>I'm happy to help you with a review of a bank application, but I'll need more specific information about the application.
+                         Could you provide more details about the features, functionality, user experience, and any specific aspects you'd like to be included in the review?</p>
+                </div>
+            </div>
+          
+            <div class="testimonial-box">
+          
+                <div class="box-top">
+         
+                    <div class="profile">
+          
+                        <div class="profile-img">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyz-77X11MoGE22xVjjPhbpW6lPj6I0SkcTQ&usqp=CAU" />
+                        </div>
+             
+                        <div class="name-user">
+                            <strong>J.K Rowling</strong>
+                            <span>@jkrowling</span>
+                        </div>
+                    </div>
+                    
+                    <div class="reviews">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                </div>
+              
+                <div class="client-comment">
+                    <p>I'm happy to help you with a review of a bank application, but I'll need more specific information about the application. Could you provide more details about the features, functionality, user experience, and any specific aspects you'd like to be included in the review?.</p>
+                </div>
+            </div>
+        
+            <div class="testimonial-box">
+     
+                <div class="box-top">
+            
+                    <div class="profile">
+            
+                        <div class="profile-img">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3aDHlzgAfDG4_gGi4P2MKfwY-dqaqLwH-kPuyfU240-BZd32D5NsX_NSbdj6l1gg0OQU&usqp=CAU" />
+                        </div>
+               
+                        <div class="name-user">
+                            <strong>Harry Potter</strong>
+                            <span>@DanielRedclief</span>
+                        </div>
+                    </div>
+                 
+                    <div class="reviews">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="far fa-star"></i>
+                    </div>
+                </div>
+              
+                <div class="client-comment">
+                    <p>I'm happy to help you with a review of a bank application, but I'll need more specific information about the application. Could you provide more details about the features, functionality, user experience, and any specific aspects you'd like to be included in the review?.</p>
+                </div>
+            </div>
+      
+            <div class="testimonial-box">
+                
+                <div class="box-top">
+                    
+                    <div class="profile">
+                     
+                        <div class="profile-img">
+                            <img src="https://cdn-icons-png.flaticon.com/512/168/168734.png" />
+                        </div>
+                      
+                        <div class="name-user">
+                            <strong>Oliva</strong>
+                            <span>@Olivaadward</span>
+                        </div>
+                    </div>
+                   
+                    <div class="reviews">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="far fa-star"></i>
+                    </div>
+                </div>
+                
+                <div class="client-comment">
+                    <p>I'm happy to help you with a review of a bank application, but I'll need more specific information about the application. Could you provide more details about the features, functionality, user experience, and any specific aspects you'd like to be included in the review?.</p>
+                </div>
+            </div>
+            <div class="testimonial-box-container">
+         
+            <div class="testimonial-box">
+        
+                <div class="box-top">
+         
+                    <div class="profile">
+             
+                        <div class="profile-img">
+                            <img src="https://cdn-icons-png.flaticon.com/512/168/168726.png" />
+                        </div>
+           
+                        <div class="name-user">
+                            <strong>Touseeq Ijaz</strong>
+                            <span>@touseeqijazweb</span>
+                        </div>
+                    </div>
+                 
+                    <div class="reviews">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="far fa-star"></i>
+                    </div>
+                </div>
+     
+                <div class="client-comment">
+                    <p>I'm happy to help you with a review of a bank application, but I'll need more specific information about the application. Could you provide more details about the features, functionality, user experience, and any specific aspects you'd like to be included in the review?</p>
+                </div>
+            </div>
+          
+            <div class="testimonial-box">
+          
+                <div class="box-top">
+         
+                    <div class="profile">
+          
+                        <div class="profile-img">
+                            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ34oWSjT7nZW71se6wyMBGZ6NGRfM7KbXY_Y2iKdKAt6s6nyRGE5l-1BXS30FBXyE8WJw&usqp=CAU" />
+                        </div>
+             
+                        <div class="name-user">
+                            <strong>J.K Rowling</strong>
+                            <span>@jkrowling</span>
+                        </div>
+                    </div>
+                    
+                    <div class="reviews">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                    </div>
+                </div>
+              
+                <div class="client-comment">
+                    <p>I'm happy to help you with a review of a bank application, but I'll need more specific information about the application. Could you provide more details about the features, functionality, user experience, and any specific aspects you'd like to be included in the review?.</p>
+                </div>
+            </div>
+        
+           
+
+            </div>
+      
+            
+            </div>
+       
+        
+    </section>
+</body>
+</html>
+  )
+}
+export default Review
