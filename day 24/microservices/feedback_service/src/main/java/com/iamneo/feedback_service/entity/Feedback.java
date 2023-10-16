d205d5beb668be41eba2a2dea39a43fc0b2eee7d
Abiranjani <110535764+Abiranjani@users.noreply.github.com>
2023-10-16 11:06:36 +0530

    Add files via upload

---
+++ day 24/microservices/feedback_service/src/main/java/com/iamneo/feedback_service/entity/Feedback.java
@@ -0,0 +1,26 @@
+package com.iamneo.feedback_service.entity;
+
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="feedbacks")
+public class Feedback {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private String feedbackId;
+	private String email;
+	private String feedback;
+}
